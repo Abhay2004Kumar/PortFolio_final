55ad86b08e0c9db84d95d4caf0cd1edc21c8db57
Abhay2004Kumar <abs220502@gmail.com>
2024-12-21 16:08:40 +0530

    fix bug

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -32,7 +32,7 @@ function Footer() {
            
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/abhay-kumar-74b16124a//"
+                href="https://www.linkedin.com/in/abhay-kumar-74b16124a/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
